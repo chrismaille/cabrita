8fbc9011e2420800601ddf933c5d2bd793952aa2
Chris Maillefaud <chrismaillefaud@gmail.com>
2018-05-17 11:03:46 -0300

    fix: [WIP] Add tests

--- cabrita/tests/test_dockerInspect.py
+++ cabrita/tests/test_dockerInspect.py
@@ -7,8 +7,7 @@ from cabrita.tests import INSPECT_DJANGO_CONTAINER, INSPECT_DJANGO_IMAGE
 
 class TestDockerInspect(TestCase):
 
-    @mock.patch('cabrita.abc.utils.run_command', return_value=INSPECT_DJANGO_CONTAINER)
-    def setUp(self, *mocks):
+    def setUp(self):
         command = CabritaCommand(
             cabrita_path='./examples/config/cabrita-v2.yml',
             compose_path=(),
@@ -20,7 +19,8 @@ class TestDockerInspect(TestCase):
         command.prepare_dashboard()
         self.docker = command.dashboard.all_boxes[-1].docker
 
-    def test_inspect(self):
+    @mock.patch('cabrita.abc.utils.run_command', return_value=INSPECT_DJANGO_CONTAINER)
+    def test_inspect(self, *mocks):
         result_dict = {
             'name': 'django',
             'ports': '↘ 8081 8090',
@@ -28,6 +28,7 @@ class TestDockerInspect(TestCase):
             'style': 'success',
             'theme': None
         }
+        self.docker.run = mocks[0]
         self.docker.inspect('django')
         self.assertDictEqual(self.docker.status('django'), result_dict)
 
@@ -39,23 +40,29 @@ class TestDockerInspect(TestCase):
         test_name = self.docker._get_container_name('django')
         self.assertEqual(test_name, 'examples_django_1')
 
-    def test__get_inspect_data(self):
+    @mock.patch('cabrita.abc.utils.run_command', return_value=INSPECT_DJANGO_CONTAINER)
+    def test__get_inspect_data(self, *mocks):
         test_name = self.docker._get_container_name('django')
+        self.docker.run = mocks[0]
         test_data = self.docker._get_inspect_data(test_name)
         self.assertEqual(test_data, json.loads(INSPECT_DJANGO_CONTAINER)[0])
 
-    def test__define_status(self):
+    @mock.patch('cabrita.abc.utils.run_command', return_value=INSPECT_DJANGO_CONTAINER)
+    def test__define_status(self, *mocks):
         test_name = self.docker._get_container_name('django')
+        self.docker.run = mocks[0]
         test_data = self.docker._get_inspect_data(test_name)
         test_stats, test_style, test_theme = self.docker._define_status(test_data)
         self.assertEqual(test_stats, 'Running')
         self.assertEqual(test_style, 'success')
         self.assertEqual(test_theme, None)
 
+    @mock.patch('cabrita.abc.utils.run_command', return_value=INSPECT_DJANGO_CONTAINER)
     @mock.patch('cabrita.abc.utils.run_command', return_value=INSPECT_DJANGO_IMAGE)
-    def test__need_build(self, *mocks):
+    def test__need_build(self, image_mock, container_mock):
         service_name = 'django'
         test_name = self.docker._get_container_name(service_name)
+        self.docker.run = container_mock
         test_data = self.docker._get_inspect_data(test_name)
-        self.docker.run = mocks[0]
+        self.docker.run = image_mock
         self.assertFalse(self.docker._need_build(service_name, test_data))
--- cabrita/tests/test_gitInspect.py
+++ cabrita/tests/test_gitInspect.py
@@ -1,15 +1,61 @@
-from unittest import TestCase
+from typing import Union
+from unittest import TestCase, mock
+
+from cabrita.command import CabritaCommand
+
+
+def return_git_result(*args, **kwargs) -> Union[str, bool]:
+    command = args[0]
+    if 'describe' in command:
+        return '2.0.1'
+    if 'rev-parse' in command:
+        return '457ac8c'
+    if 'branch' in command:
+        return '* master'
+    if 'status' in command:
+        return \
+            """
+            ## 2.0...origin/2.0 [behind 1]
+             M cabrita/tests/test_dockerInspect.py
+             M cabrita/tests/test_gitInspect.py
+             M requirements.txt
+            """
+    else:
+        return False
 
 
 class TestGitInspect(TestCase):
-    def test_get_git_revision_from_path(self):
-        self.fail()
 
-    def test_get_git_revision(self):
-        self.fail()
+    def setUp(self):
+        command = CabritaCommand(
+            cabrita_path='./examples/config/cabrita-v2.yml',
+            compose_path=(),
+            version='test'
+        )
+        self.assertTrue(command.has_a_valid_config)
+        command.read_compose_files()
+        self.assertTrue(command.has_a_valid_compose)
+        command.prepare_dashboard()
+        self.git = command.dashboard.all_boxes[-1].git
 
-    def test_get_behind_state(self):
-        self.fail()
+    @mock.patch('cabrita.abc.utils.run_command', side_effect=return_git_result)
+    def test_get_git_revision_from_path(self, *mocks):
+        self.git.run = mocks[0]
+        test_string = self.git.get_git_revision_from_path(path='/', show_branch=True)
+        self.assertEqual(test_string, u'✎ 2.0.1 ⑂ master@457ac8c')
+
+    @mock.patch('cabrita.abc.utils.run_command', side_effect=return_git_result)
+    def test_get_git_revision(self, *mocks):
+        self.git.run = mocks[0]
+        test_revision = self.git.get_git_revision('django')
+        self.assertEqual(test_revision, u'✎ 2.0.1 ⑂ 457ac8c')
+
+    @mock.patch('os.path.isdir', return_value=True)
+    @mock.patch('cabrita.abc.utils.run_command', side_effect=return_git_result)
+    def test_get_behind_state(self, *mocks):
+        self.git.run = mocks[0]
+        test_behind = self.git.get_behind_state('/')
+        self.assertEqual(test_behind, 'OK')
 
     def test_inspect(self):
         self.fail()
